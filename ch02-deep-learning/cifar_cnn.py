d52e9f9b7f6925a2891a4e47de591b1e5e00703f
ohmycloud <ohmycloudy@gmail.com>
2025-12-06 16:13:56 +0800

    add comments

--- ch02-deep-learning/cifar_cnn.py
+++ ch02-deep-learning/cifar_cnn.py
@@ -5,27 +5,46 @@ from tensorflow.keras import layers, models
 from tensorflow.keras import optimizers
 import matplotlib.pyplot as plt
 
+# 加载 CIFAR-10 数据集
+# x_train 和 x_test 分别是形状为 [50000, 32, 32, 3] 和 [10000, 32, 32, 3] 的 numpy 数组
+# 第一个维度 50000 是数据集中图像的索引, 第二和第三维度与图像尺寸相关, 最后一个是通道(即红色、绿色或蓝色, 因为这些是RGB图像)
+# y_train 和 y_test 分别是形状为 [50000, 1] 和 [10000, 1] 的 numpy 数组
 (x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+
 NUM_CLASSES = 10
+
+# 缩放每个图像, 使像素通道值介于 0 到 1 之间
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
 
+# 对标签进行 one-hot 编码, y_train 和 y_test 分别是形状为 [50000, 10] 和 [10000, 10] 的 numpy 数组
 y_train = utils.to_categorical(y_train, NUM_CLASSES)
 y_test = utils.to_categorical(y_test, NUM_CLASSES)
 
+# 使用 Sequential 模型构建我们的多层感知机
 model = models.Sequential([
     layers.Flatten(input_shape=(32, 32, 3)),
     layers.Dense(200, activation='relu'),
     layers.Dense(150, activation='relu'),
     layers.Dense(10, activation='softmax'),
 ])
+print(model.summary())
 
+# 使用函数式 API 构建我们的多层感知机
+# Input 层是网络的入口点, 以元组形式告知网络每个数据元素的预期形状
+# 请注意，我们未指定批次大小；这并非必要，因为我们可以同时将任意数量的
+# 图像传递到 Input 层。我们不需要在Input 层定义中显式声明批次大小。
 input_layer = layers.Input(shape=(32, 32, 3))
+# 使用 Flatten 层将输入层展平为向量, 这将生成一个长度为 3072 的向量 (32 x 32 x 3)
+# 我们这样做的原因是后续的Dense 层要求其输入是展平的，而非多维数组。
 x = layers.Flatten()(input_layer)
+# Dense 层包含特定数量的单元，这些单元全连接到前一层。
+# 将输入通过两个 Dense 层，第一层有 200 个单元，第二层有 150 个单元，两者都使用 ReLU 激活函数
 x = layers.Dense(units=200, activation='relu')(x)
 x = layers.Dense(units=150, activation='relu')(x)
 output_layer = layers.Dense(units=10, activation='softmax')(x)
 model = models.Model(input_layer, output_layer)
+# 检查网络每一层的形状
 print(model.summary())
 
 opt = optimizers.Adam(learning_rate=0.0005)
